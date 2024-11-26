4471265502584c1dc3def19419902cc4e9ed61c5
Deepanshu <141805113+devdeep0@users.noreply.github.com>
2024-11-26 23:24:38 +0530

    first

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+const { title } = require('process');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./pages/**/*.{js,ts,jsx,tsx}",
+  "./components/**/*.{js,ts,jsx,tsx}",
+  "./app/**/*.{ts,tsx}",
+  "./src/**/*.{ts,tsx}",],
+  theme: {
+    extend: {
+      fontFamily:{
+        title:["Staatliches", "sans-serif"],
+        zk :["Epilogue", "sans-serif" ],
+        baloo :["Baloo Da 2", 'sans-serif'],
+      }
+    },
+  },
+  plugins: [],
+}
+
